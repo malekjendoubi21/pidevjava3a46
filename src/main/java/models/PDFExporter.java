167e269af2e6ca0a5f9925c413112f30efe9efd1
mehdi <mohamedmehdi.khamassi@esprit.tn>
2024-04-28 09:03:00 +0100

    7th commit

--- src/main/java/models/PDFExporter.java
+++ src/main/java/models/PDFExporter.java
@@ -5,6 +5,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 import java.awt.*;
 import java.io.IOException;
@@ -18,63 +19,69 @@ public class PDFExporter {
             document = new PDDocument();
             PDPage page = new PDPage();
             document.addPage(page);
-
             PDPageContentStream contentStream = new PDPageContentStream(document, page);
-
             float margin = 50;
             float yStart = page.getMediaBox().getHeight() - margin;
             float tableWidth = page.getMediaBox().getWidth() - 2 * margin;
+
             float yPosition = yStart;
+            String title = "liste de réclamations";
+            int titleFontSize = 46;
+            float titleWidth = PDType1Font.HELVETICA_BOLD.getStringWidth(title) / 1000 * titleFontSize;
+            float titleHeight = PDType1Font.HELVETICA_BOLD.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * titleFontSize;
+            float titleX = (page.getMediaBox().getWidth() - titleWidth) / 2;
+            float titleY = yStart - titleHeight;
+
+            contentStream.beginText();
+            contentStream.setFont(PDType1Font.HELVETICA_BOLD, titleFontSize);
+            contentStream.newLineAtOffset(titleX, titleY);
+            contentStream.showText(title);
+            contentStream.endText();
+
+            yStart = titleY - margin;
+            yPosition = yStart;
+
+
+            String imagePath = "C:/Users/Mega-PC/Desktop/Workshop/src/main/resources/list.png";
+            PDImageXObject image = PDImageXObject.createFromFile(imagePath, document);
+            float imageWidth = 100;
+            float imageHeight = 100;
+            float xImagePosition = margin+150;
+            float yImagePosition = yPosition - imageHeight;
+            contentStream.drawImage(image, xImagePosition, yImagePosition, imageWidth, imageHeight);
+            yPosition -= imageHeight + 10;
 
 
             String[] headers = {"ID", "Sujet", "Contenu"};
             float tableHeight = 20f;
             float rowHeight = 15f;
-            float tableWidthMargin = (tableWidth - 2 * margin) / headers.length;
             float cellMargin = 5f;
-
-            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
-            drawTable(contentStream, yStart, tableWidth, cellMargin, headers, tableHeight, rowHeight);
-
-
+            drawTable(contentStream, yPosition, tableWidth, cellMargin, headers, tableHeight, rowHeight);
+            yPosition -= (tableHeight + 5);
             contentStream.setFont(PDType1Font.HELVETICA, 12);
-            drawTableData(contentStream, yStart, tableWidth, cellMargin, data, tableHeight, rowHeight);
-
+            drawTableData(contentStream, yPosition, tableWidth, cellMargin, data, tableHeight, rowHeight);
             contentStream.close();
-
-
             document.save("list.pdf");
             document.close();
-
-            System.out.println("PDF genere !");
-
+            System.out.println("PDF generated!");
         } catch (IOException e) {
             e.printStackTrace();
-            System.err.println("Erreur lors de la génération du PDF : " + e.getMessage());
+            System.err.println("Error while generating PDF: " + e.getMessage());
         }
     }
 
     private static void drawTable(PDPageContentStream contentStream, float yStart, float tableWidth,
                                   float cellMargin, String[] headers, float tableHeight, float rowHeight) throws IOException {
-        // Set font and font size
+
         contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
-
-        // Calculate initial x-position
         float xPosition = cellMargin;
-
-        // Draw headers horizontally
         for (String header : headers) {
-            // Begin text
             contentStream.beginText();
             contentStream.newLineAtOffset(xPosition, yStart - tableHeight);
             contentStream.showText(header);
             contentStream.endText();
-
-            // Update x-position for the next header
             xPosition += tableWidth / headers.length;
         }
-
-        // Draw horizontal line under headers
         contentStream.setLineWidth(1f);
         contentStream.moveTo(cellMargin, yStart - tableHeight);
         contentStream.lineTo(cellMargin + tableWidth, yStart - tableHeight);
@@ -88,15 +95,12 @@ public class PDFExporter {
         float yPosition = yStart;
         for (reclamation item : data) {
             if (yPosition - rowHeight <= 0) {
-
                 PDPage newPage = new PDPage();
                 document.addPage(newPage);
                 contentStream.close();
-
                 contentStream = new PDPageContentStream(document, newPage);
                 yPosition = yStart;
             }
-
             contentStream.beginText();
             contentStream.newLineAtOffset(cellMargin, yPosition - rowHeight);
             contentStream.showText(String.valueOf(item.getId()));
@@ -106,7 +110,6 @@ public class PDFExporter {
             contentStream.showText(String.valueOf(item.getContenu()));
             contentStream.newLineAtOffset(tableWidth / 4, 0);
             contentStream.endText();
-
             yPosition -= rowHeight;
         }
     }
